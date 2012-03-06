212975a43f5ad9ef846870d338096afd4374bb1b
Zac McCormick <zac.mccormick@gmail.com>
2012-03-05 23:07:55 -0500

    Updated TileMill automation to work with TileMill 0.9.x

--- src/automill.sh
+++ src/automill.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-nice -n19 node /Applications/TileMill.app/Contents/Resources/index.js export $1 $2 --log=1 --bbox=$3 --minzoom=$4 --maxzoom=$5
+# Usage
+# automill.sh project outputfile bbox minzoon maxzoom
 
 # project name
 # output file (no mbtiles extension)
@@ -8,3 +9,10 @@ nice -n19 node /Applications/TileMill.app/Contents/Resources/index.js export $1 
 # min zoom
 # max zoome
 
+TILEMILL_BIN=/Applications/TileMill.app/Contents/Resources
+
+case $($TILEMILL_BIN/node -v) in
+  v0\.4\.[0-9]*) nice -n19 $TILEMILL_BIN/node $TILEMILL_BIN/index.js \
+    export $1 $2 --format=mbtiles --log=1 --bbox=$3 --minzoom=$4 --maxzoom=$5;;
+  *) echo Currently TileMill requires node 0.4.x. You are using $(node -v) >&2 && exit
+esac
